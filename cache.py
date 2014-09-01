45f09258f02a2f5690c81bb21b200e1a2d114a7a
Tim Babych <tim.babych@gmail.com>
2014-09-01 18:10:31 +0300

    pep8 spaces instead of tabs

--- cache.py
+++ cache.py
@@ -6,16 +6,15 @@ import os
 cache_dir = os.path.expanduser('~/.zapys/entries/')
 
 if not os.path.isdir(cache_dir):
-	os.makedirs(cache_dir)
-	
+    os.makedirs(cache_dir)
+
 def put(id, text):
-	f = open(cache_dir+str(id)+'.text', 'w')
-	f.write(text)
-	f.close()
-	
+    f = open(cache_dir+str(id)+'.text', 'w')
+    f.write(text)
+    f.close()
+
 def get(id):
-	return open(cache_dir+str(id)+'.text').read()
+    return open(cache_dir+str(id)+'.text').read()
 
 def get_temp():
-	return open(cache_dir+'entry.html', 'w')
-	
+    return open(cache_dir+'entry.html', 'w')
